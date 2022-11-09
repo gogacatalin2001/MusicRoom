2136d28542cf8ed548386a360a515c0785c55ae4
Catalin Goga <catasua2@gmail.com>
2022-11-09 23:06:23 +0200

    Tutorial #10 done - add leave room option

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -16,7 +16,8 @@ export default class HomePage extends Component {
     super(props);
     this.state = {
       roomCode: null,
-    }
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -51,17 +52,29 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
+  }
+
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/" render={() => {
-            return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`}/>) : (this.renderHomePage())
-          }}>
-          </Route>
+            return this.state.roomCode ? (
+              <Redirect to={`/room/${this.state.roomCode}`} /> ) : ( this.renderHomePage() );
+            }} 
+          />
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route 
+            path="/room/:roomCode" 
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />
+            }} 
+          />
         </Switch>
       </Router>
     );
